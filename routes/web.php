e3d9039f208b104a1fe93bd170d8b8114c791547
atkaa92 <atkaa92@gmail.com>
2018-03-25 02:49:09 +0400

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Auth::routes();
 Route::get('/logout', 'Auth\LoginController@logout');
 Route::post('/sentMail', 'MainController@sentMail');
-
 
 Route::group(
     [
